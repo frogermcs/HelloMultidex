233544b79ec22ac52feac46590e61ab06e0506d2
froger_mcs <froger.mcs@gmail.com>
2014-11-02 14:52:55 +0100

    enable multidex support

---
+++ app/src/main/java/io/github/froger/hellomultidex/HelloMultiDexApplication.java
@@ -0,0 +1,13 @@
+package io.github.froger.hellomultidex;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by froger_mcs on 02.11.14.
+ */
+public class HelloMultiDexApplication extends MultiDexApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
