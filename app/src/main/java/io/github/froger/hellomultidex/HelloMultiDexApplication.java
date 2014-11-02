e4269b257f51e9f815fc986c455f5e350d0bbd1f
froger_mcs <froger.mcs@gmail.com>
2014-11-02 19:57:05 +0100

    prevent from moving multidex library to secondary .dex files and invoking not loaded classes in Application

--- app/src/main/java/io/github/froger/hellomultidex/HelloMultiDexApplication.java
+++ app/src/main/java/io/github/froger/hellomultidex/HelloMultiDexApplication.java
@@ -6,8 +6,15 @@ import android.support.multidex.MultiDexApplication;
  * Created by froger_mcs on 02.11.14.
  */
 public class HelloMultiDexApplication extends MultiDexApplication {
+
     @Override
     public void onCreate() {
         super.onCreate();
+        new Runnable() {
+            @Override
+            public void run() {
+                //Your code
+            }
+        }.run();
     }
 }
